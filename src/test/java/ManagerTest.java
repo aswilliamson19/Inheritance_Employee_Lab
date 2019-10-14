7ca5da6e7a1e1ecc82d7d5e8980bb94f3a16285e
Amanda Williamson <aselliott4@aol.co.uk>
2019-10-14 14:55:21 +0100

    Parent class created

---
+++ src/test/java/ManagerTest.java
@@ -0,0 +1,49 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.Employee;
+import staff.management.Manager;
+
+import static org.junit.Assert.assertEquals;
+
+public class ManagerTest {
+
+    private static final double DELTA = 0.01;
+    private Manager manager;
+    private Employee employee;
+
+    @Before
+    public void before(){
+        manager = new Manager("Chris", "JL456789A", 35000.00, "Top Techs");
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Chris", manager.getName());
+    }
+
+    @Test
+    public void canGetNiNumber(){
+        assertEquals("JL456789A", manager.getNiNumber());
+    }
+
+    @Test
+    public void canGetSalary(){
+        assertEquals(35000.00, manager.getSalary(), DELTA);
+    }
+
+    @Test
+    public void canGetDeptName(){
+        assertEquals("Top Techs", manager.getDeptName());
+    }
+
+    @Test
+    public void canRaiseManagerSalary(){
+        assertEquals(35055.5, manager.raiseSalary(55.5), DELTA);
+    }
+
+    @Test
+    public void canPayManagerBonus(){
+        assertEquals(350, manager.payBonus(), DELTA);
+    }
+
+}
